3007ba90fbe1dd5ae01ee31f1457c8b1052c290e
RomulloSousa1 <romullosousa@outlook.com.br>
2024-10-11 15:04:41 -0300

    Edit index.js

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
-  const name = process.env.NAME || "World";
+  const name = "World";
   res.send(`Hello ${name}!`);
-=======
-  res.send("Hello World 52!");
->>>>>>> 2b41d6613daede0b1cf6213e927501b3ffd32626
 });
 
-const port = parseInt(process.env.PORT || "3000");
+const port = parseInt("3000");
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
